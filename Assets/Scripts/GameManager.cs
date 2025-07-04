8db48a12a1f015d2eb53ced3c8cfb39fc7e54d2f
Insung Park <sleepwithoutadream@gmail.com>
2025-07-04 12:00:29 +0900

    hp slider lvl system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] private Slider timerBarSlider;
+    [SerializeField] private Text levelValueText;
     public Transform board;
     public GameObject card;
-
-    public int currentLevel = 1;
+    private float maxTime = 30.0f;
+    private float currentTime;
+    private int startLevel = 1;
+    public int currentLevel;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-        }    
-        
+        }
+
     }
 
     void Start()
     {
+        currentLevel = startLevel;
         board.GetComponent<CardSpawner>().SetCard(currentLevel);
+        currentTime = maxTime;
+        timerBarSlider.maxValue = maxTime;
+        timerBarSlider.value = currentTime;
     }
 
     void Update()
     {
-        
+        currentTime -= Time.deltaTime;
+        timerBarSlider.value = currentTime;
+        levelValueText.text = currentLevel.ToString();
     }
-
 }
